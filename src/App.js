2413b50c17d5f81bbae93d0d1eac11d302a729fd
mpdiaz <mpdiaz@amazon.com>
2021-07-15 22:47:49 -0500

    wip

--- src/App.js
+++ src/App.js
@@ -1,25 +1,143 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer, useEffect, useState } from 'react';
+import './App.css'
+
+import Form from './Form'
+import Header from './Header'
+
+import { Hub, Auth } from 'aws-amplify'
+import { FaSignOutAlt, BiData} from 'react-icons/fa'
+import {API} from 'aws-amplify'
+import Amplify from 'aws-amplify'
+import awsmobile from './aws-exports'
+
+Amplify.configure(awsmobile);
+
+const initialUserState = { user: null, loading: true }
+
 
 function App() {
+
+  const [userState, dispatch] = useReducer(reducer, initialUserState)
+  const [formState, updateFormState] = useState('base')
+  
+  useEffect(() => {
+    // set listener for auth events
+    Hub.listen('auth', (data) => {
+      const { payload } = data
+      if (payload.event === 'signIn') {
+        setImmediate(() => dispatch({ type: 'setUser', user: payload.data }))
+        updateFormState('base')
+      }
+      // this listener is needed for form sign ups since the OAuth will redirect & reload
+      if (payload.event === 'signOut') {
+        setTimeout(() => dispatch({ type: 'setUser', user: null }), 350)
+      }
+    })
+    // we check for the current user unless there is a redirect to ?signedIn=true 
+    if (!window.location.search.includes('?signedin=true')) {
+      checkUser(dispatch)
+    }
+  }, [])
+
+  // This renders the custom form
+  if (formState === 'email') {
+    return (
+      <div style={styles.appContainer}>
+        <Header updateFormState={updateFormState} />
+        <Form />
+      </div>
+      )
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={styles.appContainer}>
+      <Header updateFormState={updateFormState} />
+      {
+        userState.loading && (
+          <div style={styles.body}>
+            <p>Loading...</p>
+          </div>
+        )
+      }
+      {
+        !userState.user && !userState.loading && (
+          <Buttons
+            updateFormState={updateFormState}
+          />
+        )
+      }
+      {
+        userState.user && userState.user.signInUserSession && (
+          <div style={styles.body}>
+            <h4>
+              Welcome {userState.user.signInUserSession.idToken.payload.email}
+            </h4>
+          </div>
+        )
+      }
     </div>
   );
 }
 
-export default App;
+function reducer (state, action) {
+  switch(action.type) {
+    case 'setUser':  
+      return { ...state, user: action.user, loading: false }
+    case 'loaded':
+      return { ...state, loading: false }
+    default:
+      return state
+  }
+}
+
+const styles = {
+  appContainer: {
+    paddingTop: 85,
+  },
+  loading: {
+    
+  },
+  button: {
+    marginTop: 15,
+    width: '100%', 
+    maxWidth: 250,
+    marginBottom: 10,
+    display: 'flex',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    padding: '0px 16px',
+    borderRadius: 2,
+    boxShadow: '0px 1px 3px rgba(0, 0, 0, .3)',
+    cursor: 'pointer',
+    outline: 'none',
+    border: 'none',
+    minHeight: 40
+  },
+  text: {
+    color: 'white',
+    fontSize: 14,
+    marginLeft: 10,
+    fontWeight: 'bold'
+  },
+  signOut: {
+    //backgroundColor: 'black'
+    backgroundColor: '#FF9900'
+  },
+  footer: {
+    fontWeight: '600',
+    padding: '0px 25px',
+    textAlign: 'right',
+    color: 'rgba(0, 0, 0, 0.6)'
+  },
+  anchor: {
+    color: 'rgb(255, 153, 0)',
+    textDecoration: 'none'
+  },
+  body: {
+    padding: '0px 30px',
+    height: '78vh'
+  }
+}
+
+export default App
