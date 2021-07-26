d6eebc31c517e688d496d9ed5829bd4cf44eeeff
mpdiaz <mpdiaz@amazon.com>
2021-07-26 08:13:07 -0500

    wip

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
 
   const [userState, dispatch] = useReducer(reducer, initialUserState)
   const [formState, updateFormState] = useState('base')
-  const data;
+  const data = {}
   
   useEffect(() => {
     // set listener for auth events
